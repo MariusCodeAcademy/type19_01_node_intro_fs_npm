776a7b2faeec686ec1c441760469fc4ca8787d67
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-13 09:31:14 +0200

    add

--- intro.js
+++ intro.js
@@ -6,3 +6,14 @@ function sum(sk1, sk2) {
 }
 
 sum(15, 45);
+
+// parasyti funkcija kuri suskaiciuoja argumentu gauto masyvo vidurki
+// ir iskonsolina rezultata
+function getAvg(arr) {
+  const avg = arr.reduce((total, sk) => total + sk / arr.length, 0);
+  console.log('avg ===', avg);
+  return avg;
+}
+// getAvg([1,2,3]) => 2
+// iskviesti su node ir pamatyti rez
+getAvg([1, 2, 3]);
