46bfeb3e17bb18cc2945c86f70de1a6a8cfb3820
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-13 10:15:11 +0200

    module

--- intro.js
+++ intro.js
@@ -1,10 +1,6 @@
 'use strict';
 console.log('intro.js file was loaded');
 
-function sum(sk1, sk2) {
-  console.log(sk1 + sk2);
-}
-
 sum(15, 45);
 
 // parasyti funkcija kuri suskaiciuoja argumentu gauto masyvo vidurki
---
+++ modules/sum.js
@@ -0,0 +1,8 @@
+'use strict';
+console.log('sum.js file was loaded');
+
+function sum(n1, n2) {
+  console.log('sum', n1 + n2);
+}
+
+sum(15, 45);
